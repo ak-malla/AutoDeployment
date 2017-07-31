e1cee98c41c6ea7de08c646bebfcf455bf5cb5b2
AK <animalla@cisco.com>
2017-07-31 16:25:44 -0700

    Some Changes

--- deploy/main.py
+++ deploy/main.py
@@ -18,31 +18,26 @@ cmd = "calc"
 
 vmList = ["10.22.160.85"]
 for vm in vmList:
-	WBXTF.WBXTFExecCmd(vm,cmd)
-
+    WBXTF.WBXTFExecCmd(vm,cmd)
     #Copying of the AutoIt Library
     #python /home/ak/Documents/webex-systemtest-performance-pylib/deploy/deployAutoIT.py
-
     print "Copied and Zip extracted all the files remotely"
     print "Sleeping for 10"
     time.sleep(10)
     # Starting the AutoIt Application
     #wbxtfclient 10.22.160.88 process.Run\(java -jar C:\\AutoItWebexClient\\AutoItWebexClient31.0\\webexclient-1.0.jar\)
     WBXTF.WBXTFExecCmd(vm,"java -jar C:\\AutoItWebexClient\\AutoItWebexClient31.0\\webexclient-1.0.jar")
-
     print "Sleeping for 2"
     time.sleep(2)
     #Starting the Node and Hub
     #Hub
     print "Starting the Hub"
     #wbxtfclient 10.22.160.88 process.Run\(C:\\Users\\admin\\selenium\\start-hub.bat\)
-
     print "Sleeping for 2"
     time.sleep(2)
     #Node
     print "Starting the Node"
     #wbxtfclient 10.22.160.88 process.Run\(C:\\Users\\admin\\selenium\\start-node.bat\)
-
     echo "Sleeping for 2"
     time.sleep(2)
     #Starting the restclient jar
