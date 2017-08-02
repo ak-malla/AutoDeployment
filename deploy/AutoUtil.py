f6e03cc99803ca07afb200382b3cf60369b54599
AK <animalla@cisco.com>
2017-08-01 17:42:57 -0700

    Changes in the Util

--- deploy/AutoUtil.py
+++ deploy/AutoUtil.py
@@ -1,9 +1,23 @@
-from wbxtf import WBXTF
-import urllib2
 import json
 import time
+import urllib2
+import os
+
+from wbxtf import WBXTF
 
 __author__ = "AK"
+
+'''
+ This Python Script helps to remotely update/run all the list of the Selenium IP's with latest builds
+ and restarting Applications
+ Featuring:
+ 1. Updating of the AutoIt Library versions
+ 2. Restarting all of the Applications
+ 3. Monitoring all the Java Application to the idle count value 4
+ 4. Monitoring all the Selenium and End Point IP's
+ 5. Checking and Updating the Windows
+
+'''
 
 
 # The Below Method takes 2 Parameters one Pool IP and Other is Selenium or EndPoint
@@ -24,6 +38,45 @@ def getIPs(IP, type):
         outLst.append(jObject['ip'])
     return outLst
 
+
+def executeCmdNotify(result):
+    nTotalTask = len(result)
+    nSuccess = 0
+    nFailed = 0
+    nRunning = nTotalTask
+    for key in result:
+        if result[key]["finish"] == True:
+            if result[key]["result"] == True:
+                nSuccess += 1
+                nRunning -= 1
+            else:
+                nFailed += 1
+                nRunning -= 1
+
+    outputString = "Total:%d Success:%d Failed:%d Running:%s"
+    loginfo = outputString % (nTotalTask, nSuccess, nFailed, nRunning)
+    WBXTFLogInfo(loginfo)
+
+
+# Support Multithreading
+def executeCmdByThread(machineList, function):
+    actionPool = WBXTFActionPool(8)
+    actionPool.setthreadFinishedNotifyFunc(executeCmdNotify)
+    for machine in machineList:
+        actionPool.putAction(None, function, machine)
+    actionPool.waitComplete()
+
+
+# Ping Test all the IPs and return IP list not reachable
+def pingTestMonitor(IP, type):
+    outList = getIPs(IP, type)
+    returnList = []
+    for vm in outList:
+        if os.system("ping -c 1 "+vm) == 0:
+            print "Host "+vm+" appears to be up"
+        else:
+            returnList.append(vm)
+    return returnList
 
 # This Method helps to copy the latest AutoIt Library files into the remote windows system
 def copyZipPackageAndUnZip(source, fileName, vm):
@@ -52,16 +105,18 @@ def copyWindows(vm):
         WBXTF.WBXTFCopyFileToFile("local", "/home/ak/PythonScript/AutoDeployment/deploy/WUA_DownloadInstall.vbs", vm,
                                   "C:\AutoItWebexClient\\WUA_DownloadInstall.vbs")
 
-#Get Count of all the Java Process currently running
+
+# Get Count of all the Java Process currently running
 def getJavaProcessCount(vm):
     cmd = "tasklist | find /C \"java.exe\""
-    res = WBXTF.WBXTFExecCmdReturn(vm,cmd)
+    res = WBXTF.WBXTFExecCmdReturn(vm, cmd)
     return res["result"]["Result"]["fileList"][0]["data"]
 
-#This method will help in kill and restart all the java application
+
+# This method will help in kill and restart all the java application
 def startApplications(vm):
-    #Killing all the java process if any active
-    res = WBXTF.WBXTFExecCmdReturn(vm,r"taskkill /F /IM java.exe") 
+    # Killing all the java process if any active
+    res = WBXTF.WBXTFExecCmdReturn(vm, r"taskkill /F /IM java.exe")
     time.sleep(2)
     print res
     # Starting the AutoIt Application
@@ -89,21 +144,26 @@ def startApplications(vm):
     res = WBXTF.WBXTFExecCmdWithDir(vm, "java -jar restclient-1.0.jar", "", "C:\AutoItWebexClient T32.4")
     print "%s : %s " % (vm, res)
 
-#This method will run as cron Job monitor the IP, if there is any drop in Application count, and restart if needed
+
+# This method will run as cron Job monitor the IP, if there is any drop in Application count, and restart if needed
 def monitorJavaProcessCount(vm):
     count = getJavaProcessCount(vm)
-    if(count < "4"):
-        print "Java Process Count miss match for VM "+vm+" ,Restarting all Java Applications"
+    if (count < "4"):
+        print "Java Process Count miss match for VM " + vm + " ,Restarting all Java Applications"
         startApplications(vm)
     else:
-        print "All the process are in idle count "+count+" state ,VM "+vm
+        print "All the process are in idle count " + count + " state ,VM " + vm
+
 
 if __name__ == '__main__':
     # vmObject = getIPs("10.22.136.39", "SEL")
     # for vm in vmObject:
     # print vm
-    #copyZipPackageAndUnZip("/home/ak/Downloads/", "AutoItWebexClient31.0.zip" , "10.22.160.85")
-    #copyWindows("10.22.160.85")
-    #startApplications("10.22.160.85")
-    #print getJavaProcessCount("10.22.160.85")
-    monitorJavaProcessCount("10.22.160.85")
+    # copyZipPackageAndUnZip("/home/ak/Downloads/", "AutoItWebexClient31.0.zip" , "10.22.160.85")
+    # copyWindows("10.22.160.85")
+    # startApplications("10.22.160.85")
+    # print getJavaProcessCount("10.22.160.85")
+    #monitorJavaProcessCount("10.22.160.85")
+    for vm in pingTestMonitor("10.22.136.39", "SEL"):
+        print vm
+
--- deploy/ping.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-while read line
-do
-ping -c 1 $line; echo $line - $?
-done < $1
